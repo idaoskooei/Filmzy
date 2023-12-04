502bfc51ea4c15940c17a49d0a0d8ca19bb10af7
idaoskooei <idaoskooei@gmail.com>
2023-12-04 17:00:20 -0500

    add uistate to category viewmodel

--- app/src/main/java/com/myapp/myapplication/home/categoryList/CategoryViewModel.kt
+++ app/src/main/java/com/myapp/myapplication/home/categoryList/CategoryViewModel.kt
@@ -4,30 +4,34 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.myapp.myapplication.home.categoryList.repo.CategoryRepository
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
 class CategoryViewModel(private val repository: CategoryRepository) : ViewModel() {
 
-    private val _errorMessage = MutableStateFlow<String>("")
-    val errorMessage: StateFlow<String> = _errorMessage.asStateFlow()
+    private val _uiState = MutableStateFlow(UiState())
+    val uiState: StateFlow<UiState> = _uiState.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.Eagerly,
+        initialValue = UiState()
+    )
 
-    private val _uiState = MutableStateFlow<List<TmdbGenre>>(emptyList())
-    val uiState: StateFlow<List<TmdbGenre>> get() = _uiState
+    data class UiState(
+        val errorMessage: String? = null,
+        val genres: List<TmdbGenre> = emptyList()
+    )
 
     init {
         viewModelScope.launch {
-            try {
-                _uiState.value = repository.getGenres()
-            } catch (e: Exception) {
-                handleError(e)
+            _uiState.update { currentState ->
+                currentState.copy(
+                    genres = repository.getGenres()
+                )
             }
         }
-    }
-
-    private fun handleError(error: Throwable) {
-        _errorMessage.value = "An error occurred: ${error.message}"
     }
 }
 
