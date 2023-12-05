e7ee54c6f8dad90e34d1c8f96df4165f9f9478e6
idaoskooei <idaoskooei@gmail.com>
2023-12-04 23:20:34 -0500

    add factory to viewmodel to prevent creating more than once instance of viewmodel

--- app/src/main/java/com/myapp/myapplication/movie/MovieDetailViewModel.kt
+++ app/src/main/java/com/myapp/myapplication/movie/MovieDetailViewModel.kt
@@ -1,6 +1,7 @@
 package com.myapp.myapplication.movie
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
 import com.myapp.myapplication.home.searchByTerm.movies.MovieResponse
 import com.myapp.myapplication.home.searchByTerm.repo.SearchRepository
@@ -23,6 +24,18 @@ class MovieDetailsViewModel(
                 _uiState.value = details
             } catch (e: Exception) {
                 e.printStackTrace()
+            }
+        }
+    }
+
+    companion object {
+        fun provideFactory(
+            repository: SearchRepository,
+            id: Int
+        ): ViewModelProvider.Factory = object : ViewModelProvider.Factory {
+            @Suppress("UNCHECKED_CAST")
+            override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                return MovieDetailsViewModel(repository, id) as T
             }
         }
     }
