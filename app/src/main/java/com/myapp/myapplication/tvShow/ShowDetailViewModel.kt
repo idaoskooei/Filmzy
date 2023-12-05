1cfef40f4ea773543754d14fb9c108aa9da51c75
idaoskooei <idaoskooei@gmail.com>
2023-12-04 23:47:19 -0500

    add factory provider to show detail viewmodel

--- app/src/main/java/com/myapp/myapplication/tvShow/ShowDetailViewModel.kt
+++ app/src/main/java/com/myapp/myapplication/tvShow/ShowDetailViewModel.kt
@@ -1,6 +1,8 @@
 package com.myapp.myapplication.tvShow
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
 import com.myapp.myapplication.home.searchByTerm.repo.SearchRepository
 import com.myapp.myapplication.home.searchByTerm.shows.TVShowResponse
@@ -23,6 +25,18 @@ class ShowDetailViewModel(
                 _uiState.value = details
             } catch (e: Exception) {
                 e.printStackTrace()
+            }
+        }
+    }
+
+    companion object {
+        fun provideFactory(
+            repository: SearchRepository,
+            id: Int
+        ): ViewModelProvider.Factory = object : ViewModelProvider.Factory {
+            @Suppress("UNCHECKED_CAST")
+            override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                return ShowDetailViewModel(repository, id) as T
             }
         }
     }
